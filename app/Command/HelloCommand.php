c2f18dcd40218f53535d3a1e173588ac68119088
Eugene <goncharovei@gmail.com>
2025-01-14 21:30:24 +0300

    Console

---
+++ app/Command/HelloCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Usage: php console app:hello
+ * Manually register each Command class in console script file
+ */
+class HelloCommand extends Command
+{
+    protected static $defaultName = 'app:hello';
+
+    protected function configure()
+    {
+        $this->setDescription('Confirms Commands can be called');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Hello World!');
+
+        return Command::SUCCESS;
+    }
+}
