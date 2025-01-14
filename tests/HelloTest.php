9943aa3ff88f6c604e4fbe7aa865ce4cca130d95
Eugene <goncharovei@gmail.com>
2025-01-14 21:23:04 +0300

    Tests

---
+++ tests/HelloTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests;
+
+use App\Hello;
+use PHPUnit\Framework\TestCase;
+
+class HelloTest extends TestCase
+{
+    public function testGetGreeting()
+    {
+        $hello = new Hello();
+
+        $this->assertSame('Hello World!', $hello->getGreeting());
+    }
+}
