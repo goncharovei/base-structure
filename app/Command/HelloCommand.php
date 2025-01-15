69c2c126b099537c0e802cb17f46e829ca1c4e20
Eugene <goncharovei@gmail.com>
2025-01-15 10:00:53 +0300

    Console: fixed the demo command

--- app/Command/HelloCommand.php
+++ app/Command/HelloCommand.php
@@ -6,17 +6,12 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-/**
- * Usage: php console app:hello
- * Manually register each Command class in console script file
- */
 class HelloCommand extends Command
 {
-    protected static string $defaultName = 'app:hello';
-
     protected function configure(): void
     {
-        $this->setDescription('Confirms Commands can be called');
+        $this->setName('app:hello')
+            ->setDescription('Demo Command');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
