d039f45dbca38b139c376a4bac68fdaceb8e0869
Eugene <goncharovei@gmail.com>
2025-01-31 20:47:21 +0300

    Foundation, http: fixed registration Request entity

--- app/Http/Controller/HomeController.php
+++ app/Http/Controller/HomeController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controller;
 
+use Foundation\Kernels\Http\Request;
+
 class HomeController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         return response('Hello World!');
     }
