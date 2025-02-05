d986f0ea8cfb43f9c24390ed8d9c99f798eb0087
Eugene <goncharovei@gmail.com>
2025-02-05 09:40:25 +0300

    Foundation: display a error

--- bootstrap/Foundation/Exception/ExceptionHttp.php
+++ bootstrap/Foundation/Exception/ExceptionHttp.php
@@ -15,7 +15,15 @@ final class ExceptionHttp implements ExceptionOutput
 
     public function show(\Throwable $exception): void
     {
-        $response = $this->view->response($this->pathTemplate, []);
+        $statusCode = $exception->getCode() ?: 500;
+        $errorText = $exception->getMessage() . ' ' . $exception->getFile() . '(' . $exception->getLine() . ').';
+        $response = $this->view->response($this->pathTemplate, [
+            'statusCode' => $statusCode,
+            'errorText' => $errorText,
+            'trace' => nl2br($exception->getTraceAsString())
+        ]);
+        $response->withStatus($statusCode);
+
         (new SapiEmitter())->emit($response);
     }
 }
